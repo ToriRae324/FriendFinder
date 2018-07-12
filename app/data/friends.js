b9b35aebd8d9a270078885799668e2cdd80e1f01
Victoria Paul <victoriapaul@CodeBook-Pro.local>
2018-07-12 18:32:38 -0400

    working app

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,20 @@
-var friends = [];
+// Stock Friends Added
+var friends = [
+{
+    name: "Tori",
+    photoURL: "https://media.licdn.com/dms/image/C4D03AQErCwiafwl7CA/profile-displayphoto-shrink_200_200/0?e=1536796800&v=beta&t=9WFSexVS48Qcn6rilHyBCTdue-8f17oO0E5rbXJY1Hg",
+    scores: [2, 3, 1, 4, 3, 2, 1, 2, 3, 2]
+},
+{
+    name: "Jeremy",
+    photoURL: "https://images.pexels.com/photos/247885/pexels-photo-247885.jpeg?auto=compress&cs=tinysrgb&h=350",
+    scores: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+},
+{
+    name: "Kimberly",
+    photoURL: "https://images.pexels.com/photos/372042/pexels-photo-372042.jpeg?auto=compress&cs=tinysrgb&h=350",
+    scores: [5, 4, 3, 2, 1, 5, 4, 3, 2, 1]
+},
+];
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,24 +7,51 @@ var friends = require("../data/friends")
 
 // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 
-router.get("/api/friends", function(req, res) {
-    return res.json(friends);
-  });
+router.get("/api/friends", function (req, res) {
+  return res.json(friends);
+});
 
 
 // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 
-router.post("/api/friends", function(req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body-parser middleware
-    var newFriend = req.body;
-  
-    console.log(newFriend);
-  
-    friends.push(newFriend);
-  
-    res.json(newFriend);
+router.post("/api/friends", function (req, res) {
+  //  retreive newFriend data
+  var newFriend = req.body;
+  var newFriendScores = req.body.scores
+  console.log(newFriend);
+
+  // parseInt newFriendScores
+  newFriendScores.forEach(score => {
+    score = parseInt(score)
   });
 
+  // Find best match
+  // Set first best match to first friend
+  var bestMatchIndex = 0;
+  var minDiff = 40;
 
-  module.exports = router
+  // go through friends array one at a time
+  for (var i = 0; i < friends.length; i++) {
+    var totalDiff = 0;
+    // check each friend
+    for (var j = 0; j < friends[i].scores.length; j++) {
+      var diff = Math.abs(newFriendScores[j] - friends[i].scores[j]);
+      totalDiff += diff;
+    };
+
+    // if there is a new minimum, update bestMatchIndex and reset min
+    if (totalDiff < minDiff) {
+      bestMatchIndex = i;
+      minDiff = totalDiff;
+    };
+
+  }
+  // return data for best match
+  res.json(friends[bestMatchIndex]);
+
+  // After match add newFriend to friends array
+  friends.push(newFriend);
+});
+
+
+module.exports = router
